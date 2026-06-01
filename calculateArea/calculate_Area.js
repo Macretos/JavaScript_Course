10f3856132c31113abb693609088afb91f8bf55d
Macretos <danielmiguelezalonso@gmail.com>
2026-06-01 17:45:57 -0400

    Funciones JS

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -4,3 +4,12 @@ function calculateArea(){
     let area = length * width;  
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+//Excercise
+function groceryTracker(){
+    grocery1 = parseFloat(document.getElementById('grocery1').value)
+    grocery2 = parseFloat(document.getElementById('grocery2').value)
+    grocery3 = parseFloat(document.getElementById('grocery3').value)
+    let total1 = grocery1 + grocery2 + grocery3
+    document.getElementById('total1').innerText = `The total of the grocery purchase is: ${total1}`;
+}
